a4090c277ffecf15bca2690c2c3402342302db18
emadadel999 <emadadel1234@gmail.com>
2024-02-26 22:24:26 -0500

    some ui fixes

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,45 @@
 <script setup lang="ts">
-import Carousel from './components/carousel/carousel.vue';
-
+import Carousel from "./components/carousel/carousel.vue";
 
 const slides = [
-    { src: '/slides/1.png'},
-    { src: '/slides/2.png'},
-    { src: '/slides/3.png'},
-    { src: '/slides/4.png'},
-    { src: '/slides/5.png'},
-    { src: '/slides/6.png'},
-    { src: '/slides/7.png'},
-    { src: '/slides/8.png'},
-    { src: '/slides/9.png'},
-    { src: '/slides/10.png'},
-    { src: '/slides/11.png'},
-    { src: '/slides/12.png'},
-    { src: '/slides/13.png'},
+  { src: "/slides/1.png" },
+  { src: "/slides/2.png" },
+  { src: "/slides/3.png" },
+  { src: "/slides/4.png" },
+  { src: "/slides/5.png" },
+  { src: "/slides/6.png" },
+  { src: "/slides/7.png" },
+  { src: "/slides/8.png" },
+  { src: "/slides/9.png" },
+  { src: "/slides/10.png" },
+  { src: "/slides/11.png" },
+  { src: "/slides/12.png" },
+  { src: "/slides/13.png" },
 ];
 </script>
 
 <template>
   <div class="container">
-      <Carousel 
-          :slides-array="slides" 
-          :transition-speed="12" 
-          :auto-slide="true" 
-          :items-to-show="8"
-      >
-          <template #slide="{ src, alt }">
-              <div class="container-slide">
-                  <img
-                      :src="src"
-                      :alt="alt"
-                      width="256"
-                      height="335"
-                  />
-              </div>
-          </template>
-      </Carousel>
+    <Carousel :slides-array="slides" :transition-speed="1" :auto-slide="false" :items-to-show="4">
+      <template #slide="{ src, alt }">
+        <div class="container-slide">
+          <img :src="src" :alt="alt" width="256" height="335" />
+        </div>
+      </template>
+    </Carousel>
   </div>
 </template>
 
 <style lang="scss">
-@import './styles/main.scss';
+@import "./styles/main.scss";
 .container {
-  z-index: 900;
-  height: 50%;
   overflow: hidden;
+}
+
+.carousel-slides__wrapper {
+  background-color: khaki;
+  border-radius: 10px;
+  border: 0;
 }
 
 .container-slide {
@@ -58,13 +51,10 @@ const slides = [
 
   > picture,
   > img {
-      border-radius: 10px;
-      border: 0;
-      width: auto;
-      max-width: 100%;
-      height: auto;
-      max-height: 100%;
+    border-radius: 10px;
+    border: 0;
+    width: 20em;
+    height: 20em;
   }
 }
-
 </style>
--- src/components/carousel/carousel.vue
+++ src/components/carousel/carousel.vue
@@ -1,368 +1,422 @@
 <script setup lang="ts">
 import { onMounted, onBeforeUnmount, onUnmounted, ref, type Ref, inject, computed, nextTick } from "vue";
-import infiniteModulo from './infinite-modulo'
-import Debug from 'debug';
-const debug = Debug('wave:cmpnt:carousel');
+import infiniteModulo from "./infinite-modulo";
+import Debug from "debug";
+const debug = Debug("wave:cmpnt:carousel");
 
 interface iBreakpoint {
-    size: number;
-    itemsToShow: number;
+  size: number;
+  itemsToShow: number;
 }
 
 //TODO: validate props and handle errors
 const props = defineProps({
-    /**
-     * an array of slides to be shown.
-     * you can define how its value is shown inside the component's tags.
-     *  * @example
-     * // [
-     *  {
-     *      src: 1
-     *  },
-     *  {
-     *      src: 2
-     *  },
-     *  {
-     *      src: 3
-     *  }
-     * ]
-     */
-    slidesArray: {
-        type: Array,
-        required: true,
+  /**
+   * an array of slides to be shown.
+   * you can define how its value is shown inside the component's tags.
+   *  * @example
+   * // [
+   *  {
+   *      src: 1
+   *  },
+   *  {
+   *      src: 2
+   *  },
+   *  {
+   *      src: 3
+   *  }
+   * ]
+   */
+  slidesArray: {
+    type: Array,
+    required: true,
+  },
+  /**
+   * how many slides to show.
+   */
+  itemsToShow: {
+    type: Number,
+    default: 4,
+  },
+  /**
+   * how fast your slides move.
+   */
+  transitionSpeed: {
+    type: Number,
+    default: 5,
+  },
+  /**
+   * the way your slides move.
+   */
+  timingFunction: {
+    type: String,
+    default: "linear",
+  },
+  /**
+   * if it's true, and autoSlide is true, the slides will stop when you hover over them.
+   */
+  stopOnHover: {
+    type: Boolean,
+    default: false,
+  },
+  /**
+   * the way the slides should stop after hovering
+   * * @values "immediate", "wait"
+   */
+  stopOnHoverType: {
+    type: String,
+    default: "immediate",
+    validator(value: string) {
+      // The value must match one of these strings
+      return ["immediate", "wait"].includes(value);
     },
-    /**
-     * how many slides to show.
-     */
-    itemsToShow: {
-        type: Number,
-        default: 4,
-    },
-    /**
-     * how fast your slides move.
-     */
-    transitionSpeed: {
-        type: Number,
-        default: 5,
-    },
-    /**
-     * the way your slides move.
-     */
-    timingFunction: {
-        type: String,
-        default: 'linear'
-    },
-    /**
-     * if it's true, and autoSlide is true, the slides will stop when you hover over them.
-     */
-    stopOnHover: {
-        type: Boolean,
-        default: false,
-    },
-    /**
-     * the way the slides should stop after hovering
-     * * @values "immediate", "wait"
-     */
-    stopOnHoverType: {
-        type: String,
-        default: "immediate",
-        validator(value: string) {
-            // The value must match one of these strings
-            return ["immediate", "wait"].includes(value);
-        },
-    },
-    /**
-     * whether to automatically slide infinitely or no.
-     */
-    autoSlide: {
-        type: Boolean,
-        default: false,
-    },
-    /**
-     * when autoSlide is false, define next arrow button class
-     */
-    nextBtnClass: {
-        type: String,
-        default: "next-btn",
-    },
-    /**
-     * when autoSlide is false, define prev arrow button class
-     */
-    prevBtnClass: {
-        type: String,
-        default: "prev-btn",
-    },
-    /**
-     * when autoSlide is false, define next arrow button inline style object
-     */
-    nextBtnStyle: {
-        type: Object,
-    },
-    /**
-     * when autoSlide is false, define prev arrow button inline style object
-     */
-    prevBtnStyle: {
-        type: Object,
-    },
-    /**
-     * breakpoints to change itemsToShow based on the screensize
-     * @example
-     * [
-     *     {
-     *         size: 800,
-     *         itemsToShow: 3,
-     *     },
-     *     {
-     *         size: 740,
-     *         itemsToShow: 2,
-     *     },
-     *     {
-     *         size: 0,
-     *         itemsToShow: 1,
-     *     },
-     * ];
-     */
-    breakpoints: {
-        type: Array<iBreakpoint>,
-        default: null,
-    },
+  },
+  /**
+   * whether to automatically slide infinitely or no.
+   */
+  autoSlide: {
+    type: Boolean,
+    default: false,
+  },
+  /**
+   * when autoSlide is false, define next arrow button class
+   */
+  nextBtnClass: {
+    type: String,
+    default: "next-btn",
+  },
+  /**
+   * when autoSlide is false, define prev arrow button class
+   */
+  prevBtnClass: {
+    type: String,
+    default: "prev-btn",
+  },
+  /**
+   * when autoSlide is false, define next arrow button inline style object
+   */
+  nextBtnStyle: {
+    type: Object,
+  },
+  /**
+   * when autoSlide is false, define prev arrow button inline style object
+   */
+  prevBtnStyle: {
+    type: Object,
+  },
+  /**
+   * breakpoints to change itemsToShow based on the screensize
+   * @example
+   * [
+   *     {
+   *         size: 800,
+   *         itemsToShow: 3,
+   *     },
+   *     {
+   *         size: 740,
+   *         itemsToShow: 2,
+   *     },
+   *     {
+   *         size: 0,
+   *         itemsToShow: 1,
+   *     },
+   * ];
+   */
+  breakpoints: {
+    type: Array<iBreakpoint>,
+    default: null,
+  },
 });
 
 const slidesWrapper = ref<HTMLElement>();
 const slidesWrapperInner = ref<HTMLElement>();
 const currentSlide = ref(0);
-const windowWidth: Ref<number> = inject('windowWidth') as Ref<number>;
+const windowWidth: Ref<number> = inject("windowWidth") as Ref<number>;
 let interval: DOMHighResTimeStamp;
 let stopSliding = false;
 let speed: number, then: DOMHighResTimeStamp, elapsed: number;
 
 const itemsToShow = computed(() => {
-    let result = props.itemsToShow;
-    if (props.breakpoints && props.breakpoints.length > 0) {
-        const breakpoint = props.breakpoints.find(b => b.size <= windowWidth?.value);
-        if (breakpoint) result = breakpoint.itemsToShow;
-    }
-    return result;
+  let result = props.itemsToShow;
+  if (props.breakpoints && props.breakpoints.length > 0) {
+    const breakpoint = props.breakpoints.find((b) => b.size <= windowWidth?.value);
+    if (breakpoint) result = breakpoint.itemsToShow;
+  }
+  return result;
 });
 
 const clonedArr = computed(() => {
-    let result: any[] = [];
-    //replicate arr until first "itemsToShow" elements show up in the same order at the end.
-    const resultLength = itemsToShow.value * props.slidesArray.length + itemsToShow.value;
+  let result: any[] = [];
+  //replicate arr until first "itemsToShow" elements show up in the same order at the end.
+  const resultLength = itemsToShow.value * props.slidesArray.length + itemsToShow.value;
 
-    let i = 0;
-    let j = 0;
-    while (i <= resultLength - 1) {
-        result.push(props.slidesArray[j]);
-        i++;
-        j++;
-        if (j === props.slidesArray.length) j = 0;
-    }
-    return result;
+  let i = 0;
+  let j = 0;
+  while (i <= resultLength - 1) {
+    result.push(props.slidesArray[j]);
+    i++;
+    j++;
+    if (j === props.slidesArray.length) j = 0;
+  }
+  return result;
 });
 
 const numOfViews = computed(() => Math.ceil(clonedArr.value.length / itemsToShow.value));
 const slideWidth = computed(() => {
-    const result = 100 / itemsToShow.value;
-    return `${result}%`;
+  const result = 100 / itemsToShow.value;
+  return `${result}%`;
 });
 
 onMounted(() => {
-    debug('onMounted() - ');
-    if (props.autoSlide) {
-        document.addEventListener('visibilitychange', onPageVisibilityChange);
-        startAnimation();
-    }
+  debug("onMounted() - ");
+  if (props.autoSlide) {
+    document.addEventListener("visibilitychange", onPageVisibilityChange);
+    startAnimation();
+  }
 });
 
 onBeforeUnmount(() => {
-    debug('onBeforeUnmount() - ');
-    document.removeEventListener('visibilitychange', onPageVisibilityChange);
+  debug("onBeforeUnmount() - ");
+  document.removeEventListener("visibilitychange", onPageVisibilityChange);
 });
 
 onUnmounted(() => {
-    debug('onUnmounted() - ');
-    window.cancelAnimationFrame(interval);
+  debug("onUnmounted() - ");
+  window.cancelAnimationFrame(interval);
 });
 
 function onPageVisibilityChange() {
-    debug('onPageVisibilityChange() - ', document.hidden);
-    if (document.hidden)
-        stopAnimation();
-    else
-        startAnimation();
+  debug("onPageVisibilityChange() - ", document.hidden);
+  if (document.hidden) stopAnimation();
+  else startAnimation();
 }
 
-
 function startAnimation() {
-    stopSliding = false;
-    speed = props.transitionSpeed * 1000;
-    then = 0;
-    slideRight();
-    interval = window.requestAnimationFrame(animate);
+  stopSliding = false;
+  speed = props.transitionSpeed * 1000;
+  then = 0;
+  slideRight();
+  interval = window.requestAnimationFrame(animate);
 }
 
 function animate(now: DOMHighResTimeStamp) {
-    if (stopSliding) return;
-    if (then === 0) then = now;
+  if (stopSliding) return;
+  if (then === 0) then = now;
 
-    elapsed = now - then;
-    if (elapsed >= speed) {
-        then = now;
-        slideRight();
-    }
-    interval = window.requestAnimationFrame(animate);
+  elapsed = now - then;
+  if (elapsed >= speed) {
+    then = now;
+    slideRight();
+  }
+  interval = window.requestAnimationFrame(animate);
 }
 
 function stopAnimation() {
-    stopSliding = true;
-    window.cancelAnimationFrame(interval);
-    slidesWrapperInner.value?.classList.add("carousel__disable-transition");
-    currentSlide.value = 0;
-    slidesWrapper.value?.style.setProperty("--current-slide", `${currentSlide.value}`);
-    slidesWrapperInner.value?.offsetHeight;
-    slidesWrapperInner.value?.classList.remove("carousel__disable-transition");
-    slidesWrapperInner.value?.offsetHeight;
+  stopSliding = true;
+  window.cancelAnimationFrame(interval);
+  slidesWrapperInner.value?.classList.add("carousel__disable-transition");
+  currentSlide.value = 0;
+  slidesWrapper.value?.style.setProperty("--current-slide", `${currentSlide.value}`);
+  slidesWrapperInner.value?.offsetHeight;
+  slidesWrapperInner.value?.classList.remove("carousel__disable-transition");
+  slidesWrapperInner.value?.offsetHeight;
 }
 function pauseAnimation(stopTransitionClass: string) {
-    slidesWrapperInner.value?.classList.add(stopTransitionClass);
-    slidesWrapperInner.value?.offsetHeight;
-    window.cancelAnimationFrame(interval);
-    slidesWrapperInner.value?.classList.remove(stopTransitionClass);
-    slidesWrapperInner.value?.offsetHeight;
+  slidesWrapperInner.value?.classList.add(stopTransitionClass);
+  slidesWrapperInner.value?.offsetHeight;
+  window.cancelAnimationFrame(interval);
+  slidesWrapperInner.value?.classList.remove(stopTransitionClass);
+  slidesWrapperInner.value?.offsetHeight;
 }
 
 //TODO: "immediate" type is not working as expected
 function onMouseEnter() {
-    debug('onMouseEnter() - ');
-    if (props.autoSlide && props.stopOnHover) {
-        if (props.stopOnHoverType === "wait") {
-            pauseAnimation('carousel__pause-transition');
-        } else {
-            pauseAnimation("carousel__disable-transition");
-        }
+  debug("onMouseEnter() - ");
+  if (props.autoSlide && props.stopOnHover) {
+    if (props.stopOnHoverType === "wait") {
+      pauseAnimation("carousel__pause-transition");
+    } else {
+      pauseAnimation("carousel__disable-transition");
     }
+  }
 }
 
 function onMouseLeave() {
-    debug('onMouseLeave() - ');
-    if (props.autoSlide && props.stopOnHover) startAnimation();
+  debug("onMouseLeave() - ");
+  if (props.autoSlide && props.stopOnHover) startAnimation();
 }
 
 function slideRight() {
-    console.log('currentSlide.value', currentSlide.value);
-    console.log('numOfViews.value', numOfViews.value);
-    if (currentSlide.value === numOfViews.value - 1) {
-        resetSliding(next);
-    } else {
-        next();
-    }
+  console.log("currentSlide.value", currentSlide.value);
+  console.log("numOfViews.value", numOfViews.value);
+  if (currentSlide.value === numOfViews.value - 1) {
+    resetSliding(next);
+  } else {
+    next();
+  }
 }
 
 function slideLeft() {
-    if (currentSlide.value === 0) {
-        resetSliding(prev);
-    } else {
-        prev();
-    }
+  if (currentSlide.value === 0) {
+    resetSliding(prev);
+  } else {
+    prev();
+  }
 }
 
 function resetSliding(move: Function) {
-    slidesWrapperInner.value?.classList.add("carousel__disable-transition");
-    move();
-    slidesWrapperInner.value?.offsetHeight; //flush css to cause a reflow.
-    slidesWrapperInner.value?.classList.remove("carousel__disable-transition");
-    move();
+  slidesWrapperInner.value?.classList.add("carousel__disable-transition");
+  move();
+  slidesWrapperInner.value?.offsetHeight; //flush css to cause a reflow.
+  slidesWrapperInner.value?.classList.remove("carousel__disable-transition");
+  move();
 }
 
 function next() {
-    currentSlide.value = infiniteModulo(currentSlide.value + 1, numOfViews.value);
-    if (currentSlide.value <= 1) {
-        slidesWrapperInner.value?.offsetHeight; //flush css to cause a reflow.
-    }
-    slidesWrapper.value?.style.setProperty("--current-slide", `${currentSlide.value}`);
+  currentSlide.value = infiniteModulo(currentSlide.value + 1, numOfViews.value);
+  if (currentSlide.value <= 1) {
+    slidesWrapperInner.value?.offsetHeight; //flush css to cause a reflow.
+  }
+  slidesWrapper.value?.style.setProperty("--current-slide", `${currentSlide.value}`);
 }
 
 function prev() {
-    currentSlide.value = infiniteModulo(currentSlide.value - 1, numOfViews.value);
-    if (currentSlide.value <= 1) {
-        slidesWrapperInner.value?.offsetHeight; //flush css to cause a reflow.
-    }
-    slidesWrapper.value?.style.setProperty("--current-slide", `${currentSlide.value}`);
+  currentSlide.value = infiniteModulo(currentSlide.value - 1, numOfViews.value);
+  if (currentSlide.value <= 1) {
+    slidesWrapperInner.value?.offsetHeight; //flush css to cause a reflow.
+  }
+  slidesWrapper.value?.style.setProperty("--current-slide", `${currentSlide.value}`);
 }
-
 </script>
 
 <template>
-    <button v-if="!autoSlide" type="button" :style="prevBtnStyle" :class="`carousel__btn ${prevBtnClass}`"
-        @click="slideLeft">
-        <slot name="prev"></slot>
+  <div class="carousel__container">
+    <button v-if="!autoSlide" type="button" :style="prevBtnStyle" :class="`carousel__btn ${prevBtnClass}`" @click="slideLeft">
+      <slot name="prev">
+        <img src="/arrow-left.svg" alt="arrow left" />
+      </slot>
     </button>
-    <div class="carousel-slides__wrapper" ref="slidesWrapper" :style="{ transition: `all ${props.transitionSpeed}s ${timingFunction}` }">
-        <ul class="carousel-slides__wrapper-inner" ref="slidesWrapperInner"
-            :style="{ transition: `all ${props.transitionSpeed}s ${timingFunction}` }" @mouseenter="onMouseEnter"
-            @mouseleave="onMouseLeave">
-            <li v-for="(slide, index) in clonedArr" :key="index" :id="`${index + 1}`" :style="{ flex: `0 0 ${slideWidth}` }"
-                class="slide">
-                <slot name="slide" v-bind="slide"></slot>
-            </li>
-        </ul>
+    <div class="carousel-slides__wrapper" ref="slidesWrapper">
+      <ul
+        class="carousel-slides__wrapper-inner"
+        ref="slidesWrapperInner"
+        :style="{ transition: `all ${props.transitionSpeed}s ${timingFunction}` }"
+        @mouseenter="onMouseEnter"
+        @mouseleave="onMouseLeave"
+      >
+        <li v-for="(slide, index) in clonedArr" :key="index" :id="`${index + 1}`" :style="{ flex: `0 0 ${slideWidth}` }" class="slide">
+          <slot name="slide" v-bind="slide"> </slot>
+        </li>
+      </ul>
     </div>
-    <button type="button" v-if="!autoSlide" :style="nextBtnStyle" :class="`carousel__btn ${nextBtnClass}`"
-        @click="slideRight">
-        <slot name="next"></slot>
+    <button type="button" v-if="!autoSlide" :style="nextBtnStyle" :class="`carousel__btn ${nextBtnClass}`" @click="slideRight">
+      <slot name="next">
+        <img src="/arrow-right.svg" alt="arrow right" />
+      </slot>
     </button>
+  </div>
 </template>
 
-<style lang="scss">
-@import '../../styles/main.scss';
+<style scoped lang="scss">
+@import "../../styles/main.scss";
+.carousel__container {
+  display: flex;
+}
 .carousel-slides__wrapper {
-    width: fit-content;
-    min-width: 80%;
-    height: 100%;
-    --current-slide: 0;
-    overflow: hidden;
-    list-style-type: none;
-    display: flex;
+  width: fit-content;
+  min-width: 80%;
+  height: 100%;
+  --current-slide: 0;
+  overflow: hidden;
+  list-style-type: none;
+  display: flex;
 }
 
 .carousel__btn {
-    min-width: 10%;
-    width: 30%;
-    z-index: 1200;
-    cursor: pointer;
-    text-align: center;
-    border: 0 solid transparent;
-    background: transparent;
+  min-width: 10%;
+  width: 30%;
+  z-index: 1200;
+  cursor: pointer;
+  text-align: center;
+  border: 0 solid transparent;
+  background: transparent;
+  > picture,
+  > img {
+    border-radius: 10px;
+    border: 0;
+    width: auto;
+    max-width: 100%;
+    height: auto;
+    max-height: 100%;
+  }
 }
 
 .carousel-slides__wrapper-inner {
-    width: 100%;
-    list-style-type: none;
-    padding: 0;
-    margin: 0;
-    display: flex;
-    transform: translateX(calc(-100% * var(--current-slide)));
+  width: 100%;
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  transform: translateX(calc(-100% * var(--current-slide)));
 
-    .slide {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        max-height: 100%;
+  .slide {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    max-height: 100%;
+    * {
+      max-width: 100%;
     }
+  }
 }
 
 .carousel__disable-transition {
-    -webkit-transition: none !important;
-    -moz-transition: none !important;
-    -o-transition: none !important;
-    transition: none !important;
+  -webkit-transition: none !important;
+  -moz-transition: none !important;
+  -o-transition: none !important;
+  transition: none !important;
 }
 
 .carousel__pause-transition {
-    -webkit-transition: all 5s ease !important;
-    -moz-transition: all 5s ease !important;
-    -o-transition: all 5s ease !important;
-    transition: all 5s ease !important;
+  -webkit-transition: all 5s ease !important;
+  -moz-transition: all 5s ease !important;
+  -o-transition: all 5s ease !important;
+  transition: all 5s ease !important;
+}
+
+.gg-arrow-right-o {
+  box-sizing: border-box;
+  position: relative;
+  display: block;
+  width: 22px;
+  height: 22px;
+  border: 2px solid;
+  transform: scale(var(--ggs, 1));
+  border-radius: 20px;
+}
+
+.gg-arrow-right-o::after,
+.gg-arrow-right-o::before {
+  content: "";
+  display: block;
+  box-sizing: border-box;
+  position: absolute;
+  right: 4px;
+}
+
+.gg-arrow-right-o::after {
+  width: 6px;
+  height: 6px;
+  border-top: 2px solid;
+  border-right: 2px solid;
+  transform: rotate(45deg);
+  bottom: 6px;
+}
+
+.gg-arrow-right-o::before {
+  width: 10px;
+  height: 2px;
+  bottom: 8px;
+  background: currentColor;
 }
 </style>
